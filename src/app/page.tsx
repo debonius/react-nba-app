9b56a98fbd594895b4f09f66d6779247a0045b03
Francky Iside <francky@tuta.io>
2023-04-29 08:21:47 +0200

    configuring fetch of game scores - in progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,30 @@
 // import Image from 'next/image'
-import styles from './page.module.css'
-import Navigation from '../app/components/shared/Navigation';
+// import styles from './page.module.css'
+import Navigation from '../app/components/shared/Navigation'
+
+const url = 'https://free-nba.p.rapidapi.com/games?page=0&per_page=25';
+const options = {
+  method: 'GET',
+  headers: {
+    'content-type': 'application/octet-stream',
+    'X-RapidAPI-Key': '7b95667d0emsh8d9d96107126432p1809fdjsn526f18e74399',
+    'X-RapidAPI-Host': 'free-nba.p.rapidapi.com'
+  }
+};
+
+function getScores() {
+  console.log('called getScores()');
+  try {
+    const response = fetch(url, options);
+    // const result = response.text();
+    console.warn(response);
+    // console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getScores();
 
 function WelcomeMessage() {
   return (
