f8c5de83d79b6ef5104d16f89fe2389060f0e1cb
Francky Iside <francky@tuta.io>
2023-04-29 07:41:29 +0200

    organize styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 // import Image from 'next/image'
-// import styles from './page.module.css'
+import styles from './page.module.css'
 import Navigation from '../app/components/shared/Navigation';
 
 function WelcomeMessage() {
   return (
-    <p>Check last NBA scores, stay update about teams and players.
+    <p className='welcome-message'>Check last NBA scores, stay update about teams and players.
     </p>
   )
 }
