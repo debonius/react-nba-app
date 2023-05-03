a9fe4fd55a928bf948bfe58d6c1f029ea79e826b
debonius <francky@tuta.io>
2023-05-03 23:21:01 +0200

    add months, show last page of scores

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,16 @@ import { useState, useEffect } from 'react';
 import Navigation from '../app/components/shared/Navigation'
 import Game from './api/game';
 
-const URL = 'https://free-nba.p.rapidapi.com/games?per_page=25';
+const URL = 'https://free-nba.p.rapidapi.com/games?page=693&per_page=1000';
 const OPTIONS = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Key': 'e97c778be5msh166dd90d82df4dep1025b4jsnd4d0e762bcc5',
   }
 };
+
+const MONTHS = ["January", "February", "March", "April", "May", "June", "July",
+  "August", "September", "October", "November", "December"];
 
 function WelcomeMessage() {
   return (
@@ -36,6 +39,7 @@ function LatestScores() {
           return new Date(b.date).getTime() - new Date(a.date).getTime();
         }));
         setGotScores(true);
+        console.log(obj)
         console.log(obj.data)
       });
   }
@@ -52,7 +56,9 @@ function LatestScores() {
             <div>
               {score.home_team.abbreviation} {score.home_team_score} vs {score.visitor_team_score} {score.visitor_team.abbreviation}
             </div>
-            <span>{new Date(score.date).getFullYear()}/{new Date(score.date).getMonth()}/{new Date(score.date).getDay()}</span>
+            <span>
+              {MONTHS[new Date(score.date).getMonth()]} {new Date(score.date).getDate()}, {new Date(score.date).getFullYear()}
+            </span>
           </li>
         ))}
       </ul>
