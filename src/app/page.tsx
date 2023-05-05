4c260c9fb323bb971ce9330022abdf96eb1f1bd3
debonius <francky@tuta.io>
2023-05-06 00:07:22 +0200

    remove unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
 'use client';
-// import styles from './page.module.css'
-import { useState, useEffect } from 'react';
 import Navigation from '../app/components/shared/Navigation'
 import ScoreList from '../app/components/shared/ScoreList'
-import Game from './api/types/games';
-import Meta from './api/types/meta';
 
 function WelcomeMessage() {
   return (
