0231619d2dec8b18f4b83cb9e958025d321ab6a9
debonius <francky@tuta.io>
2023-05-05 23:12:28 +0200

    add new route players

---
+++ src/app/players/page.tsx
@@ -0,0 +1,10 @@
+import Navigation from '../components/shared/Navigation';
+
+export default function Scores() {
+    return (
+        <>
+            <h1>Players works!</h1>
+            <Navigation />
+        </>
+    )
+}
