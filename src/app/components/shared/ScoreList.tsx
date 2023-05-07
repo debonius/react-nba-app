7e3b928c64d733d761dc4d9f1fcc58c6362ee177
debonius <francky@tuta.io>
2023-05-07 09:36:43 +0200

    remove comments

--- src/app/components/shared/ScoreList.tsx
+++ src/app/components/shared/ScoreList.tsx
@@ -20,22 +20,14 @@ export default function ScoreList() {
     const [meta, setMeta] = useState<Meta>();
     const [scores, setScores] = useState<Games>([]);
     const [receivedScore, setReceivedScore] = useState<Boolean>(false);
-    // const [perPage, setPerPage] = useState<Number>(10);
-    // const [totalPages, setTotalPages] = useState<Number>(0);
-    // const [totalCount, setTotalCount] = useState<Number>(0);
-    // const [currentPage, setCurrentPage] = useState<Number>(1);
 
     function fetchScores() {
         fetch(`https://free-nba.p.rapidapi.com/games`, OPTIONS)
             .then(response => response.json())
             .then(obj => {
-                // setMeta({ ...obj.meta });
                 setMeta(obj.meta);
                 console.warn('meta: ', obj.meta);
                 console.warn('meta: ', meta);
-                // setPerPage(obj.meta.per_page);
-                // setTotalPages(obj.meta.total_pages);
-                // setTotalCount(obj.meta.total_count);
             })
             .finally(() => {
                 console.log(meta?.total_pages);
@@ -51,19 +43,6 @@ export default function ScoreList() {
                     });
             });
     }
-
-    // const fetchScores = () => {
-    //     const URL = `https://free-nba.p.rapidapi.com/games?page=${meta?.total_pages}&per_page=${meta?.per_page}`;
-    //     fetch(URL, OPTIONS)
-    //         .then(response => response.json())
-    //         .then(obj => {
-    //             setScores(obj.data.sort((a: Game, b: Game) => {
-    //                 return new Date(b.date).getTime() - new Date(a.date).getTime();
-    //             }));
-    //             setReceivedScore(true);
-    //         });
-
-    // };
 
     useEffect(() => {
         fetchScores();
@@ -101,11 +80,6 @@ export default function ScoreList() {
                 <Pagination
                     meta={meta}
                     fetchScores={fetchScores}
-                // setPerPage={setPerPage}
-                // perPage={perPage}
-                // setCurrentPage={setCurrentPage}
-                // currentPage={currentPage}
-                // totalPages={totalPages}
                 />
             }
         </div >
