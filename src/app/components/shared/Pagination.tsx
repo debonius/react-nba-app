f200bd80201f495f80fe732a780394adea1cad85
debonius <francky@tuta.io>
2023-05-07 09:35:32 +0200

    fixing pagination and sorting - in progress

--- src/app/components/shared/Pagination.tsx
+++ src/app/components/shared/Pagination.tsx
@@ -1,29 +1,23 @@
 import * as React from 'react';
 import TablePagination from '@mui/material/TablePagination';
 
-export default function Pagination({ meta, setPerPage, perPage, getScores, setCurrentPage, currentPage, totalPages }) {
-    const [page, setPage] = React.useState(totalPages);
-    const [rowsPerPage, setRowsPerPage] = React.useState(perPage);
+export default function Pagination({ meta, fetchScores }) {
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
     const handleChangePage = (
         event: React.MouseEvent<HTMLButtonElement> | null,
         newPage: number,
     ) => {
         setPage(newPage);
-        setCurrentPage(newPage)
-        console.log('nba currentPage: ', currentPage);
-        console.log('mui page: ', page);
-        console.log('mui newPage: ', newPage);
-        getScores();
+        fetchScores();
     };
 
     const handleChangeRowsPerPage = (
         event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
     ) => {
         setRowsPerPage(parseInt(event.target.value, 10));
-        setPerPage(rowsPerPage);
-        // setPage(totalPages);
-        getScores(perPage);
+        fetchScores();
     };
 
     return (
--- src/app/components/shared/ScoreList.tsx
+++ src/app/components/shared/ScoreList.tsx
@@ -8,56 +8,71 @@ import Meta from '../../api/types/meta';
 const MONTHS = ["January", "February", "March", "April", "May", "June", "July",
     "August", "September", "October", "November", "December"];
 
+const OPTIONS = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'e97c778be5msh166dd90d82df4dep1025b4jsnd4d0e762bcc5',
+    }
+};
+
 export default function ScoreList() {
     type Games = Game[];
-    const [scores, setScores] = useState<Games>([]);
     const [meta, setMeta] = useState<Meta>();
-    const [perPage, setPerPage] = useState<Number>(10);
-    const [totalPages, setTotalPages] = useState<Number>(0);
-    const [totalCount, setTotalCount] = useState<Number>(0);
-    const [currentPage, setCurrentPage] = useState<Number>(1);
-    const [fetchSuccess, setFetchSuccess] = useState<Boolean>(false);
+    const [scores, setScores] = useState<Games>([]);
+    const [receivedScore, setReceivedScore] = useState<Boolean>(false);
+    // const [perPage, setPerPage] = useState<Number>(10);
+    // const [totalPages, setTotalPages] = useState<Number>(0);
+    // const [totalCount, setTotalCount] = useState<Number>(0);
+    // const [currentPage, setCurrentPage] = useState<Number>(1);
 
-    const getScores = () => {
-        const URL = `https://free-nba.p.rapidapi.com/games?page=${totalPages}&per_page=${perPage}`;
-        const OPTIONS = {
-            method: 'GET',
-            headers: {
-                'X-RapidAPI-Key': 'e97c778be5msh166dd90d82df4dep1025b4jsnd4d0e762bcc5',
-            }
-        };
+    function fetchScores() {
         fetch(`https://free-nba.p.rapidapi.com/games`, OPTIONS)
             .then(response => response.json())
             .then(obj => {
+                // setMeta({ ...obj.meta });
                 setMeta(obj.meta);
+                console.warn('meta: ', obj.meta);
                 console.warn('meta: ', meta);
-                setPerPage(obj.meta.per_page);
-                setTotalPages(obj.meta.total_pages);
-                setTotalCount(obj.meta.total_count);
-
+                // setPerPage(obj.meta.per_page);
+                // setTotalPages(obj.meta.total_pages);
+                // setTotalCount(obj.meta.total_count);
+            })
+            .finally(() => {
+                console.log(meta?.total_pages);
+                console.log(meta?.per_page);
+                const URL = `https://free-nba.p.rapidapi.com/games?page=${meta?.total_pages}&per_page=${25}`;
                 fetch(URL, OPTIONS)
                     .then(response => response.json())
                     .then(obj => {
-                        // setScores(obj.data.sort((a: Game, b: Game) => {
-                        //     return new Date(b.date).getTime() - new Date(a.date).getTime();
-                        // }));
-                        setScores(obj.data);
-                        setFetchSuccess(true);
-                        console.info('nba perPage: ', perPage);
-                        console.info('nba currentPage: ', currentPage);
+                        setScores(obj.data.sort((a: Game, b: Game) => {
+                            return new Date(b.date).getTime() - new Date(a.date).getTime();
+                        }));
+                        setReceivedScore(true);
                     });
             });
+    }
 
+    // const fetchScores = () => {
+    //     const URL = `https://free-nba.p.rapidapi.com/games?page=${meta?.total_pages}&per_page=${meta?.per_page}`;
+    //     fetch(URL, OPTIONS)
+    //         .then(response => response.json())
+    //         .then(obj => {
+    //             setScores(obj.data.sort((a: Game, b: Game) => {
+    //                 return new Date(b.date).getTime() - new Date(a.date).getTime();
+    //             }));
+    //             setReceivedScore(true);
+    //         });
 
-    };
+    // };
 
-
-    useEffect(() => getScores(), []);
+    useEffect(() => {
+        fetchScores();
+    }, []);
 
     return (
         <div className='latest-scores'>
             <ul>
-                {fetchSuccess && scores.map(score => (
+                {receivedScore && scores.map(score => (
                     <li key={score.id} className='latest-scores__row'>
                         <Image
                             src={'/img/teams/' + score.home_team.abbreviation + '.svg'}
@@ -85,12 +100,12 @@ export default function ScoreList() {
             {meta &&
                 <Pagination
                     meta={meta}
-                    setPerPage={setPerPage}
-                    perPage={perPage}
-                    getScores={getScores}
-                    setCurrentPage={setCurrentPage}
-                    currentPage={currentPage}
-                    totalPages={totalPages}
+                    fetchScores={fetchScores}
+                // setPerPage={setPerPage}
+                // perPage={perPage}
+                // setCurrentPage={setCurrentPage}
+                // currentPage={currentPage}
+                // totalPages={totalPages}
                 />
             }
         </div >
