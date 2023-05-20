21fd25878b41dbf3601a405273b95e46e5c53f31
debonius <francky@tuta.io>
2023-05-20 06:14:18 +0200

    add first and last page buttons

--- src/app/components/shared/ScoreList.tsx
+++ src/app/components/shared/ScoreList.tsx
@@ -8,6 +8,8 @@ import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import FirstPageIcon from '@mui/icons-material/FirstPage';
+import LastPageIcon from '@mui/icons-material/LastPage';
 
 const MONTHS = ["January", "February", "March", "April", "May", "June", "July",
     "August", "September", "October", "November", "December"];
@@ -23,6 +25,7 @@ export default function ScoreList() {
     type games = game[];
     const [scores, setScores] = useState<games>([]);
     const [receivedScore, setReceivedScore] = useState<boolean>(false);
+    const [lastPage, setLastPage] = useState<number>(6930);
 
     const [pageOptions, setPageOptions] = useState<pageOptions>({
         page_size: 10,
@@ -66,6 +69,24 @@ export default function ScoreList() {
         console.log('page', pageOptions.page);
     }
 
+    const handleGoToFirstPage = () => {
+        setPageOptions({
+            ...pageOptions,
+            page: 1,
+        });
+        fetchScores(pageOptions.page_size, pageOptions.page);
+        console.log('page', pageOptions.page);
+    }
+
+    const handleGoToLastPage = () => {
+        setPageOptions({
+            ...pageOptions,
+            page: lastPage,
+        });
+        fetchScores(pageOptions.page_size, pageOptions.page);
+        console.log('page', pageOptions.page);
+    }
+
     function fetchScores(pageSize: number, page: number) {
         console.log('called fetchScoresList()');
         console.log('with page: ', page);
@@ -73,9 +94,12 @@ export default function ScoreList() {
         fetch(`https://free-nba.p.rapidapi.com/games?page=${page}&per_page=${pageSize}`, OPTIONS)
             .then(response => response.json())
             .then(obj => {
-                setScores(obj.data.sort((a: game, b: game) => {
-                    return new Date(b.date).getTime() - new Date(a.date).getTime();
-                }));
+                setScores(obj.data);
+                // setScores(obj.data.sort((a: game, b: game) => {
+                //     return new Date(b.date).getTime() - new Date(a.date).getTime();
+                // }));
+                console.log('meta.total_pages: ', obj.meta.total_pages);
+                setLastPage(obj.meta.total_pages);
             }).finally(() => {
                 setReceivedScore(true);
             });
@@ -97,14 +121,35 @@ export default function ScoreList() {
                     {pageOptions.page_size > 10 && <Button onClick={subtractTen}>-10</Button>}
                     <Button onClick={addTen}>+10</Button>
                 </ButtonGroup>
-                {pageOptions.page > 1 &&
-                    <ArrowBackIosIcon
-                        onClick={handleGoToPrevPage}
-                        color='primary'
-                        className='pagination__btn-change-page'
-                    />}
-                <span>Page: {pageOptions.page > 0 && pageOptions.page}</span>
-                <ArrowForwardIosIcon className='pagination__btn-change-page' onClick={handleGoToNextPage} color='primary' />
+                <div>
+                    {pageOptions.page > 1 &&
+                        <>
+                            <FirstPageIcon
+                                onClick={handleGoToFirstPage}
+                                className='pagination__btn-change-page'
+                            />
+                            <ArrowBackIosIcon
+                                onClick={handleGoToPrevPage}
+                                color='primary'
+                                className='pagination__btn-change-page'
+                            />
+                        </>
+                    }
+                    {pageOptions.page > 0 &&
+                        <>
+                            <span>Page {pageOptions.page}</span>
+                            <ArrowForwardIosIcon
+                                className='pagination__btn-change-page'
+                                onClick={handleGoToNextPage}
+                                color='primary'
+                            />
+                            <LastPageIcon
+                                onClick={handleGoToLastPage}
+                                className='pagination__btn-change-page'
+                            />
+                        </>
+                    }
+                </div>
             </div>
             <ul>
                 {receivedScore && scores.map(score => (
