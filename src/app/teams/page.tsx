3c410ee46c8e14d2c6633849df8afa952055ae11
debonius <francky@tuta.io>
2023-05-20 07:16:05 +0200

    fetch teams

--- src/app/teams/page.tsx
+++ src/app/teams/page.tsx
@@ -1,17 +1,43 @@
-import Navigation from '../components/shared/Navigation';
+'use client';
+import { useEffect } from 'react';
 
-function AllTeams() {
+function Heading() {
     return (
         <h1>All teams</h1>
     )
 }
 
 export default function Teams() {
+
+    async function fetchTeams() {
+        const url = 'https://free-nba.p.rapidapi.com/teams?page=0';
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': '0c008c7080msh10a514646ed797cp1182abjsn21ea7e48e462',
+                'X-RapidAPI-Host': 'free-nba.p.rapidapi.com'
+            }
+        };
+
+        try {
+            const response = await fetch(url, options);
+            const result = await response.json();
+            console.log(result);
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchTeams()
+    }, [])
+
+
     return (
         <>
             <h1>Teams works!</h1>
-            <AllTeams />
-            <Navigation />
+            <Heading />
         </>
     )
 }
