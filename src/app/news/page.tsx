cfa9374f3eecee038296814e250ba57345541566
debonius <francky@tuta.io>
2023-05-28 11:37:51 +0200

    show news

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,9 +1,58 @@
+'use client';
+import { useEffect, useState } from 'react';
 import Navigation from '../components/shared/Navigation';
 
+interface news {
+    id: number,
+    source: 'string',
+    title: 'string',
+    url: 'string',
+}
+
 export default function News() {
+    const [results, setResults] = useState<news[]>([]);
+    const [receivedResults, setReceivedResults] = useState<boolean>(false);
+
+    async function fetchNews() {
+        const URL = 'https://nba-latest-news.p.rapidapi.com/articles?source=nba';
+        const OPTIONS = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': '0c008c7080msh10a514646ed797cp1182abjsn21ea7e48e462',
+                'X-RapidAPI-Host': 'nba-latest-news.p.rapidapi.com'
+            }
+        };
+
+        try {
+            const response = await fetch(URL, OPTIONS);
+            const results = await response.json();
+            console.log(results);
+            setResults(results);
+            setReceivedResults(true);
+        } catch (error) {
+            console.error(error);
+            setReceivedResults(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchNews()
+    }, [])
+
     return (
         <>
-            <h1>News works!</h1>
+            <h1>Latest news from NBA</h1>
+            <ul>
+                {
+                    results && results.map((r, index) => (
+                        <li key={index}>
+                            <h2>{r.title}</h2>
+                            <a href={r.url}>Read more</a>
+                            <span className='text--shadow'> (Source {r.source.toUpperCase()})</span>
+                        </li>
+                    ))
+                }
+            </ul>
             <Navigation />
         </>
     )
